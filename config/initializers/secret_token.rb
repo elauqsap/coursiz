008edb5bbcd58ee21803a936842da9ffa017d075
pasqualedagostino <pasquale-dagostino@uiowa.edu>
2013-11-01 13:17:39 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coursiz::Application.config.secret_token = '2785e6df2392fbd73e9ac38fc72a1d80b8d3f1b8d28e5eef50b4b287002986ff23062432f666ec39df6cdcfe155f048b9fd0dd73acaeb9f3e0f2c06b41587f10'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Coursiz::Application.config.session_store :cookie_store, key: '_coursiz_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Coursiz::Application.config.session_store :active_record_store

ad085f20baaead4032189faa061df3355b57fb02
elauqsap <pasquale-dagostino@uiowa.edu>
2013-12-09 23:26:03 -0600

    stats branch

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,10 +1,12 @@
 class StatsController < ApplicationController
+before_filter :authenticate_user!
 
   def index
-  	if !user_signed_in?
-  		redirect_to new_user_session_path
+  	if Stat.find_by_users_id(current_user.id).nil?
+  		flash[:warning] = "You currently have no Quiz Statistics"
+  		redirect_to root_path
   	else
-    	
+    	@chart = Stat.create_chart(current_user.id)
     end
   end
 
--- app/models/stat.rb
+++ app/models/stat.rb
@@ -18,17 +18,48 @@ class Stat < ActiveRecord::Base
   	end
   	@stat.c_answers = (@stat.c_answers || 0 ) + @c_answers
   	@stat.f_answers = (@stat.f_answers || 0 ) + @f_answers
-  	puts params[:difficulty]
+  	
   	case params[:difficulty].to_i
-  	when 1
-		@stat.difficulty_1 = (@stat.difficulty_1 || 0) + 1
-	when 2
-		@stat.difficulty_2 = (@stat.difficulty_2 || 0) + 1
-	when 3
-		@stat.difficulty_3 = (@stat.difficulty_3 || 0) + 1
-	end
+  	 when 1
+		  @stat.difficulty_1 = (@stat.difficulty_1 || 0) + 1
+	   when 2
+		  @stat.difficulty_2 = (@stat.difficulty_2 || 0) + 1
+	   when 3
+		  @stat.difficulty_3 = (@stat.difficulty_3 || 0) + 1
+	  end
+
   	@stat.quizzes_taken = (@stat.quizzes_taken || 0) + 1
-  	@stat.save!
+    @stat.save!
+  end
+
+  def self.create_chart(current_user)
+    @stat = Stat.find_by_users_id(current_user)
+    @chart  = LazyHighCharts::HighChart.new('pie') do |f|
+        f.chart({:defaultSeriesType=>"pie" , :margin=> [50, 200, 60, 170]} )
+        series = {
+                 :type=> 'pie',
+                 :name=> 'Browser share',
+                 :data=> [
+                    ['Correct', ((@stat.c_answers.to_f/(@stat.c_answers.to_f+@stat.f_answers.to_f)) * 100).round(2)],
+                    ['Wrong', ((@stat.f_answers.to_f/(@stat.c_answers.to_f+@stat.f_answers.to_f)) * 100).round(2)]
+                 ]
+        }
+        f.series(series)
+        f.options[:title][:text] = "Answer Ratio"
+        f.legend(:layout=> 'vertical',:style=> {:left=> 'auto', :bottom=> 'auto',:right=> '50px',:top=> '100px'}) 
+        f.plot_options(:pie=>{
+          :allowPointSelect=>true, 
+          :cursor=>"pointer" , 
+          :dataLabels=>{
+            :enabled=>true,
+            :color=>"black",
+            :style=>{
+              :font=>"13px Trebuchet MS, Verdana, sans-serif"
+            }
+          }
+        })
+    end 
+    return @chart
   end
 
 end
